e659a08c96c886ae87368d5ac1c8bcf9b8624188
Владислав Куманов <edu.kumanov@outlook.com>
2021-10-04 04:19:55 +0300

    Add project files.

---
+++ Lesson7-Practice2-PieChart/PieChartElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_Practice2_PieChart
+{
+    internal class PieChartElement
+    {
+        public string name;
+        public float value;
+
+        public PieChartElement()
+        {
+        }
+
+        public PieChartElement(string _name, float _value)
+        {
+            name = _name;
+            value = _value;
+        }
+    }
+}
